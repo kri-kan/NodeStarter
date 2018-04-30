6b9ada7c05f6ef12c4c96fa3f2214c98906cb779
krikan <krishna.kanth@artefactsys.com>
2018-04-30 12:50:01 +0530

    clean up commented code.

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -1,21 +1,10 @@
 import dotenv from "dotenv";
-import winston, { FileTransportOptions } from "winston";
-import {Logger,Transports,FileTransportInstance} from "winston";
+import winston from "winston";
+import {Logger} from "winston";
 
-// var p : FileTransportInstance;
-// var x : FileTransportOptions;
-
-// x = {filename:"debug.log",level:"debug"};
-// p = {json: true,logstash: true, colorize: true,  maxsize: null,
-//     rotationFormat: false,   zippedArchive: false,   maxFiles: null,
-//     prettyPrint: true,  label: null,   timestamp: true,
-//     eol: false,    tailable: true,    depth: null,
-//     showLevel: true,    maxRetries: 1 , new{x:""}   
-//     };
 
 const logger = new (Logger)({
     transports:[
-        // new (winston.transports.File)({filename:"debug.log",level:"debug"}),
         new (winston.transports.Coductionsole)({level : process.env.NODE_ENV === "production" ? "error" : "debug" }),
         new (winston.transports.File)({filename:"debug.log",level:"debug"})
     ]
